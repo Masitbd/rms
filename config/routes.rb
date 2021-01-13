6f830a498d2ce1442fc928d3934127dd6baec535
Murad Sarker <sarker24@gmail.com>
2021-01-13 11:33:33 +0100

    Add the category module

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#index'
 
+
+resources :item_categories
   resources :configurations do 
     collection do 
       get 'update'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_114017) do
+ActiveRecord::Schema.define(version: 2021_01_12_221118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "item_categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "restaurant_configurations", force: :cascade do |t|
     t.string "restaurant_name"
